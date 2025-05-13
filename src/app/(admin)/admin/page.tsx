1c3bcc8bda162ef1a6f051cf4397914e83bf02d1
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-05-13 10:10:05 +0100

    fix:suspenseBoundary

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,9 +1,12 @@
 import Dashboard from "./components/dashboard";
-
+import { Suspense } from "react";
+import LoadingSvg from "@/components/load";
 export default function AdminHomePage() {
   return (
     <>
+    	<Suspense fallback={<LoadingSvg/>}>
       <Dashboard />
+      </Suspense>
     </>
   );
 }
