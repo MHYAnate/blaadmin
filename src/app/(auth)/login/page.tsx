46c93dc30f3ece59af287ea1eb1d6832b47fa7e1
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-06-09 19:48:49 +0100

    email

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -570,6 +570,7 @@ type FormSchemaType = z.infer<typeof formSchema>;
 export default function LoginPage() {
   const { loginData, loginIsLoading, loginPayload } = useLogin((res: any) => {
     Storage.set("token", res?.data?.token);
+    Storage.set("email", res?.data?.user?.email ?? loginData?.email);
     window.location.href = "/admin";
   });
 
